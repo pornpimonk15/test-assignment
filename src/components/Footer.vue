4fad6d5e8fc4a375e7981f699fd668c324227002
pornpimon <pornpimonk15@gmail.com>
2022-06-25 16:53:44 +0700

    feat: create ui footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-footer class="footer">
+    <div class="footer-content">
+      <span class="px-2">© 2022 Pornpimon</span>
+      <v-icon color="white">mdi-heart</v-icon>
+    </div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.footer {
+  background: #b24592;
+  background: -webkit-linear-gradient(to right, #f15f79, #b24592);
+  background: linear-gradient(to right, #f15f79, #b24592);
+}
+.footer-content {
+  flex: 1;
+  text-align: center;
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 65px;
+}
+</style>
